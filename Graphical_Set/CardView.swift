67a364f27540dffeae93631499757c49f29abb0d
edison <edison.yangyong@gmail.com>
2018-05-25 17:33:06 -1000

    optimize drawing

--- Graphical_Set/CardView.swift
+++ Graphical_Set/CardView.swift
@@ -34,92 +34,80 @@ class CardView: UIView {
     }
     
     func draw_diamond(){
+        let path1 = UIBezierPath()
+        let path2 = UIBezierPath()
+        let path3 = UIBezierPath()
         if self.num == 1 || self.num == 3{
-            path = UIBezierPath()
-            path.move(to: CGPoint(x: bounds.minX+0.163*bounds.width, y: bounds.midY))
-            path.addLine(to: CGPoint(x: bounds.midX, y: bounds.minY+0.414*bounds.height))
-            path.addLine(to: CGPoint(x: bounds.minX+0.837*bounds.width, y: bounds.midY))
-            path.addLine(to: CGPoint(x: bounds.midX, y: bounds.minY+0.596*bounds.height))
-            draw_path()
+            d_diamond(path: path1, offset: 0 )
         }
         if self.num == 2 || self.num == 3{
-            path = UIBezierPath()
-            path.move(to: CGPoint(x: bounds.minX+0.163*bounds.width, y: bounds.midY-offset!))
-            path.addLine(to: CGPoint(x: bounds.midX, y: (bounds.minY+0.414*bounds.height)-offset!))
-            path.addLine(to: CGPoint(x: bounds.minX+0.837*bounds.width, y: bounds.midY-offset!))
-            path.addLine(to: CGPoint(x: bounds.midX, y: (bounds.minY+0.596*bounds.height)-offset!))
-           draw_path()
-            path = UIBezierPath()
-            path.move(to: CGPoint(x: bounds.minX+0.163*bounds.width, y: bounds.midY+offset!))
-            path.addLine(to: CGPoint(x: bounds.midX, y: (bounds.minY+0.414*bounds.height)+offset!))
-            path.addLine(to: CGPoint(x: bounds.minX+0.837*bounds.width, y: bounds.midY+offset!))
-            path.addLine(to: CGPoint(x: bounds.midX, y: (bounds.minY+0.596*bounds.height)+offset!))
-           draw_path()
+            d_diamond(path: path2, offset: offset!)
+            d_diamond(path: path3, offset: -offset!)
+        }
+    }
+    func d_diamond(path: UIBezierPath, offset: CGFloat){
+        path.move(to: CGPoint(x: bounds.minX+0.163*bounds.width, y: bounds.midY+offset))
+        path.addLine(to: CGPoint(x: bounds.midX, y: (bounds.minY+0.414*bounds.height)+offset))
+        path.addLine(to: CGPoint(x: bounds.minX+0.837*bounds.width, y: bounds.midY+offset))
+        path.addLine(to: CGPoint(x: bounds.midX, y: (bounds.minY+0.596*bounds.height)+offset))
+        draw_path(path:path)
+    }
+    
+    func draw_oval(){
+        let path1 = UIBezierPath()
+        let path2 = UIBezierPath()
+        let path3 = UIBezierPath()
+        if self.num == 1 || self.num == 3{
+            d_oval(path: path1, offset: 0)
+        }
+        if self.num == 2 || self.num == 3{
+            d_oval(path: path2, offset: offset!)
+            d_oval(path: path3, offset: -offset!)
+        }
+    }
+    func d_oval(path: UIBezierPath, offset: CGFloat){
+        path.move(to: CGPoint(x: bounds.minX+0.267*bounds.width, y: bounds.minY+0.6*bounds.height+offset))
+        path.addArc(withCenter: CGPoint(x: bounds.minX+0.267*bounds.width, y: bounds.midY+offset), radius: 0.1*bounds.height, startAngle: 0.5*CGFloat.pi, endAngle: 1.5*CGFloat.pi, clockwise: true)
+        path.addLine(to: CGPoint(x: bounds.minX+0.681*bounds.width, y: bounds.minY+0.4*bounds.height+offset))
+        path.addArc(withCenter: CGPoint(x: bounds.minX+0.681*bounds.width, y: bounds.midY+offset), radius: 0.1*bounds.height, startAngle: 1.5*CGFloat.pi, endAngle: -1.5*CGFloat.pi, clockwise: true)
+        draw_path(path: path)
+    }
+    
+    func draw_squiggle(){
+        let path1 = UIBezierPath()
+        let path2 = UIBezierPath()
+        let path3 = UIBezierPath()
+        if self.num == 1 || self.num == 3{
+            d_squiggle(path: path1, offset: 0)
+        }
+        if self.num == 2 || self.num == 3{
+            d_squiggle(path: path2, offset: offset!)
+            d_squiggle(path: path3, offset: -offset!)
+        }
+    }
+    func d_squiggle(path: UIBezierPath, offset: CGFloat){
+        path.move(to: CGPoint(x: bounds.minX+0.1815*bounds.width, y: bounds.minY+0.579*bounds.height+offset))
+        path.addCurve(to:  CGPoint(x: bounds.minX+0.815*bounds.width, y: bounds.minY+0.421*bounds.height+offset), controlPoint1:  CGPoint(x: bounds.minX+0.305*bounds.width, y: bounds.minY+0.162*bounds.height+offset), controlPoint2: CGPoint(x: bounds.minX+0.672*bounds.width, y: bounds.minY+0.579*bounds.height+offset))
+        path.addCurve(to:  CGPoint(x: bounds.minX+0.1815*bounds.width, y: bounds.minY+0.579*bounds.height+offset), controlPoint1:  CGPoint(x: bounds.minX+0.695*bounds.width, y: bounds.minY+0.838*bounds.height+offset), controlPoint2: CGPoint(x: bounds.minX+0.328*bounds.width, y: bounds.minY+0.421*bounds.height+offset))
+        draw_path(path: path)
+    }
+    
+    func draw_lines(){
+        for i in 0...25{
+            let line = UIBezierPath()
+            line.move(to: CGPoint(x: bounds.minX+bounds.width/25*CGFloat(i), y: bounds.minY))
+            line.addLine(to: CGPoint(x: bounds.minX+bounds.width/25*CGFloat(i), y: bounds.maxY))
+            line.lineWidth = 5.0
+            line.stroke()
         }
     }
     
-    func draw_path(){
+    func draw_path(path: UIBezierPath){
         path.close()
         path.lineWidth = 5.0
         path.stroke()
         path.fill()
-    }
-    
-    func draw_oval(){
-        if self.num == 1 || self.num == 3{
-            path = UIBezierPath()
-            path.move(to: CGPoint(x: bounds.minX+0.267*bounds.width, y: bounds.minY+0.6*bounds.height))
-            path.addArc(withCenter: CGPoint(x: bounds.minX+0.267*bounds.width, y: bounds.midY), radius: 0.1*bounds.height, startAngle: 0.5*CGFloat.pi, endAngle: 1.5*CGFloat.pi, clockwise: true)
-            path.addLine(to: CGPoint(x: bounds.minX+0.681*bounds.width, y: bounds.minY+0.4*bounds.height))
-            path.addArc(withCenter: CGPoint(x: bounds.minX+0.681*bounds.width, y: bounds.midY), radius: 0.1*bounds.height, startAngle: 1.5*CGFloat.pi, endAngle: -1.5*CGFloat.pi, clockwise: true)
-            draw_path()
-        }
-        if self.num == 2 || self.num == 3{
-            path = UIBezierPath()
-            path.move(to: CGPoint(x: bounds.minX+0.267*bounds.width, y: bounds.minY+0.6*bounds.height-offset!))
-            path.addArc(withCenter: CGPoint(x: bounds.minX+0.267*bounds.width, y: bounds.midY-offset!), radius: 0.1*bounds.height, startAngle: 0.5*CGFloat.pi, endAngle: 1.5*CGFloat.pi, clockwise: true)
-            path.addLine(to: CGPoint(x: bounds.minX+0.681*bounds.width, y: bounds.minY+0.4*bounds.height-offset!))
-            path.addArc(withCenter: CGPoint(x: bounds.minX+0.681*bounds.width, y: bounds.midY-offset!), radius: 0.1*bounds.height, startAngle: 1.5*CGFloat.pi, endAngle: -1.5*CGFloat.pi, clockwise: true)
-            draw_path()
-            path = UIBezierPath()
-            path.move(to: CGPoint(x: bounds.minX+0.267*bounds.width, y: bounds.minY+0.6*bounds.height+offset!))
-            path.addArc(withCenter: CGPoint(x: bounds.minX+0.267*bounds.width, y: bounds.midY+offset!), radius: 0.1*bounds.height, startAngle: 0.5*CGFloat.pi, endAngle: 1.5*CGFloat.pi, clockwise: true)
-            path.addLine(to: CGPoint(x: bounds.minX+0.681*bounds.width, y: bounds.minY+0.4*bounds.height+offset!))
-            path.addArc(withCenter: CGPoint(x: bounds.minX+0.681*bounds.width, y: bounds.midY+offset!), radius: 0.1*bounds.height, startAngle: 1.5*CGFloat.pi, endAngle: -1.5*CGFloat.pi, clockwise: true)
-            draw_path()
-        }
-    }
-    
-    func draw_squiggle(){
-        if self.num == 1 || self.num == 3{
-            path = UIBezierPath()
-            path.move(to: CGPoint(x: bounds.minX+0.1815*bounds.width, y: bounds.minY+0.579*bounds.height))
-            path.addCurve(to:  CGPoint(x: bounds.minX+0.815*bounds.width, y: bounds.minY+0.421*bounds.height), controlPoint1:  CGPoint(x: bounds.minX+0.305*bounds.width, y: bounds.minY+0.162*bounds.height), controlPoint2: CGPoint(x: bounds.minX+0.672*bounds.width, y: bounds.minY+0.579*bounds.height))
-            path.addCurve(to:  CGPoint(x: bounds.minX+0.1815*bounds.width, y: bounds.minY+0.579*bounds.height), controlPoint1:  CGPoint(x: bounds.minX+0.695*bounds.width, y: bounds.minY+0.838*bounds.height), controlPoint2: CGPoint(x: bounds.minX+0.328*bounds.width, y: bounds.minY+0.421*bounds.height))
-            draw_path()
-        }
-        if self.num == 2 || self.num == 3{
-            path = UIBezierPath()
-            path.move(to: CGPoint(x: bounds.minX+0.1815*bounds.width, y: bounds.minY+0.579*bounds.height-offset!))
-            path.addCurve(to:  CGPoint(x: bounds.minX+0.815*bounds.width, y: bounds.minY+0.421*bounds.height-offset!), controlPoint1:  CGPoint(x: bounds.minX+0.305*bounds.width, y: bounds.minY+0.162*bounds.height-offset!), controlPoint2: CGPoint(x: bounds.minX+0.672*bounds.width, y: bounds.minY+0.579*bounds.height-offset!))
-            path.addCurve(to:  CGPoint(x: bounds.minX+0.1815*bounds.width, y: bounds.minY+0.579*bounds.height-offset!), controlPoint1:  CGPoint(x: bounds.minX+0.695*bounds.width, y: bounds.minY+0.838*bounds.height-offset!), controlPoint2: CGPoint(x: bounds.minX+0.328*bounds.width, y: bounds.minY+0.421*bounds.height-offset!))
-            draw_path()
-            path = UIBezierPath()
-            path.move(to: CGPoint(x: bounds.minX+0.1815*bounds.width, y: bounds.minY+0.579*bounds.height+offset!))
-            path.addCurve(to:  CGPoint(x: bounds.minX+0.815*bounds.width, y: bounds.minY+0.421*bounds.height+offset!), controlPoint1:  CGPoint(x: bounds.minX+0.305*bounds.width, y: bounds.minY+0.162*bounds.height+offset!), controlPoint2: CGPoint(x: bounds.minX+0.672*bounds.width, y: bounds.minY+0.579*bounds.height+offset!))
-            path.addCurve(to:  CGPoint(x: bounds.minX+0.1815*bounds.width, y: bounds.minY+0.579*bounds.height+offset!), controlPoint1:  CGPoint(x: bounds.minX+0.695*bounds.width, y: bounds.minY+0.838*bounds.height+offset!), controlPoint2: CGPoint(x: bounds.minX+0.328*bounds.width, y: bounds.minY+0.421*bounds.height+offset!))
-            draw_path()
-        }
-    }
-    
-    func draw_lines(){
-        for i in 0...50{
-            let line = UIBezierPath()
-            line.move(to: CGPoint(x: bounds.minX+bounds.width/50*CGFloat(i), y: bounds.minY))
-            line.addLine(to: CGPoint(x: bounds.minX+bounds.width/50*CGFloat(i), y: bounds.maxY))
-            line.lineWidth = 5.0
-            line.stroke()
-        }
+        //path.addClip()
     }
     
     override func draw(_ rect: CGRect) {
@@ -137,7 +125,7 @@ class CardView: UIView {
             default: break
         }
         switch self.shading{
-        case .strip?:UIColor.white.setFill(); draw_lines()
+        case .strip?:UIColor.white.setFill();
             case .fill?: break
             case .outline?: UIColor.white.setFill()
             default: break
@@ -147,6 +135,12 @@ class CardView: UIView {
             case .diamond?: draw_diamond()
             case .oval?: draw_oval()
             default: break
+        }
+        switch self.shading{
+            case .strip?:break
+            case .fill?: break
+            case .outline?: break
+        default: break
         }
     }
 }
